ffd88854268c3baf47b380ab774b8653ca6367f1
parvin.shafiee.m@gmail.com <parvin.shafiee.m@gmail.com>
2019-10-25 14:17:46 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, you see the main menu, by selecting below numbers you will navigate to related functions:");
+
+
+            do
+            {
+                int userInput = Util.AskForInt("\n 0: Exit the program"
+                                              + "\n 1: Get ticket price"
+                                              + "\n 2: Print your entered word 10 times"
+                                              + "\n 3: Print the third word in your entered sentence");
+
+                switch (userInput)
+                {
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                    case 1:
+                        userInput = Util.AskForInt("\n 0: return to main menu"
+                                                 + "\n 1:Individual Price"
+                                                 + "\n 2: Company Price");
+                        switch (userInput)
+                        {
+                            case 0:
+                                break;
+                            case 1:
+                                userInput = Util.AskForInt("Enter your age:");
+                                calculateIndividualPrice(userInput);
+                                break;
+                            case 2:
+                                userInput = Util.AskForInt("Enter numbers of personls:");
+                                Console.WriteLine($"\n\nTotal Ticket Price for {userInput} Personals is: {calculatePersonalsPrice(userInput)}kr"); 
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+
+                    case 2:
+                        var arbitraryText = Util.AskForString("Enter an arbitray text, so it will be printed 10 times:");
+                        RepeatTenTimes(arbitraryText);
+                        break;
+                    case 3:
+                        var sentence = Util.AskForString("Enter a statement with at least 3 words:");
+                        splitSentence(sentence);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+            while (true); //Endless loop
+        }
+
+        private static void splitSentence(string sentence)
+        {
+            string[] sentenceList = sentence.Split(" ");
+            if(sentenceList.Length > 2)
+                Console.WriteLine($"\n The third word in the sentence you have entered is: {sentenceList[2]}");
+            else
+             Console.WriteLine("The sentence should have at least 3 words");                
+        }
+
+        private static void RepeatTenTimes(string arbitraryText)
+        {
+            for (int i = 0; i < 10; i++)
+                Console.Write($"{i +1}. {arbitraryText} ");
+            Console.WriteLine();
+        }
+
+        private static int calculatePersonalsPrice(int personalNumbers)
+        {
+            // List<int> personalsTicketPrice = new List<int>();
+            int personalsTicketPrice = 0;
+            int age;
+            for (int i=0; i< personalNumbers; i++)
+            {
+                age = Util.AskForInt($"\n Enter Personal{i + 1} age:");
+                personalsTicketPrice += calculateIndividualPrice(age);
+            }
+
+            return personalsTicketPrice;
+        }
+
+        private static int calculateIndividualPrice(int age)
+        {
+            int price = -1;
+
+            if (age < 20)
+            {
+                price = 80;
+                Console.WriteLine("Teenagers Price: 80kr");
+            }                
+            else if (age > 64)
+            {
+                price = 90;
+                Console.WriteLine("Pensions Price: 90kr");
+            }            
+            else
+            {
+                price = 120;
+                Console.WriteLine("Standars Price: 120kr");
+            }
+
+            return price;
+        }
+    }
+}
---
+++ Util.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment2
+{
+    //Static class cant make instances of. Exactly as the Console class for example
+    class Util
+    {
+        //internal visible in the same assembly
+        //must be static in a static class
+        //string return type
+        //AskForString method name
+        //static internal bool exitProgram = false;
+        internal static string AskForString(string prompt)
+        {
+            bool correct = true;
+            string answer; //Scope
+
+            do //Repeat
+            {
+                Console.WriteLine(prompt);
+                answer = Console.ReadLine();
+
+                //If answer is not null or empty string
+                if (!string.IsNullOrEmpty(answer))
+                {
+                    //Set bool correct to false to exit loop
+                    correct = false;
+                }
+
+            } while (correct); //until we have get a correct value
+
+            return answer; //return value
+        }
+
+        internal static int AskForInt(string prompt)
+        {
+            bool success;
+            int answer; //Scope
+
+            do //Repeat
+            {
+                string input = AskForString(prompt);
+
+                //Try to parse string to int returns bool
+                //If true exit loop
+                success = int.TryParse(input, out answer);
+
+                //If we not successfully parsed string input to int answer
+                if (!success)
+                {
+                    //Write errormessage
+                    Console.WriteLine("Wrong format only numbers");
+                }
+
+            } while (!success);
+
+            //Returns parsed string
+            return answer;
+
+            /*do //Repeat
+            {
+                string input = AskForString(prompt);
+
+                //Try to parse string to int returns bool
+                //If true exit loop
+                success = int.TryParse(input, out answer);
+
+                //If we not successfully parsed string input to int answer or the input is 0
+                if (!success || answer.Equals(0))
+                {
+                    switch (answer)
+                    {
+                        case 0:
+                            //Environment.Exit(-1);
+                            exitProgram = true;
+                            break;
+
+                        default:
+                            //Write errormessage
+                            Console.WriteLine("Wrong format only numbers");
+                            break;
+                    }
+                }
+
+            } while (!success);
+
+            //Returns parsed string
+            return answer;*/
+        }
+    }
+}
